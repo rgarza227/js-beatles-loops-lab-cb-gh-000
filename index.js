3fb548a66decc93379736c8c3976c68b9d21a604
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 16:52:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 function theBeatlesPlay(musiciansArray,instrumentsArray){
   var array
   for(let i = 0; i < musiciansArray.length; i++){
-    if(i == 0){
-      array[i] = "John Lennon plays guitar"
-    }
-    else {
       array[i] ="${musiciansArray} plays ${instrumentsArray}"
-    }
   }
   return array
 }
