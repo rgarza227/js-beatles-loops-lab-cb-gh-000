9e1770cca68ae5900fae6407ca08b52247926c4c
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 16:56:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musiciansArray,instrumentsArray){
   var array = musiciansArray
   for(let i = 0; i < musiciansArray.length; i++){
-      array[i] ="${musiciansArray} plays ${instrumentsArray}"
+      array[i] ='${musiciansArray} plays ${instrumentsArray}'
   }
   return array
 }
