510baa58ac9a8362ef6b8080bb7d8281cd961a81
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 16:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musiciansArray,instrumentsArray){
+  var array
+  for(let i = 0;i<musiciansArray.length;i++){
+    array[i] ="${musiciansArray} plays ${instrumentsArray}"
+    
+  }
+  return array
+}
