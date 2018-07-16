ede2ca8bb61742e1356efc6164199b5b32aa769a
Roberto Garza <robertg4567@hotmail.com>
2018-07-16 17:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musiciansArray,instrumentsArray){
 function johnLennonFacts(facts){
   var i = 0
   while (i < facts.length){
-    
+
     facts[i] = facts[i] + "!!!"
     i++
   }
